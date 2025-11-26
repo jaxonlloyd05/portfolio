bfde72b006876a2a96bd11a2746015f7bebe6310
jaxonlloyd05 <jaxonlloyd05@gmail.com>
2025-11-26 17:13:07 +1100

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <div>
+      <h2>fdsafds</h2>
+      <button className='btn glass'>asdasd</button>
+    </div>
+  )
+}
+
+export default App
