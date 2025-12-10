0b3c6b4566ff68100ddc3c8de153fc8d67194d65
Jaxon <jaxonlloyd05@gmail.com>
2025-12-10 14:53:07 +1100

    fixing build error

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,12 @@
-import type React from "react";
+// import type React from "react";
 
 
-const Projects: React.FC = () => {
+// const Projects: React.FC = () => {
 
 
-    return (
-        <div id="projects">
+//     return (
+//         <div id="projects">
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
