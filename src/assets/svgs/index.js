8bcd34f0a31eb31183b8b79cb0a76b3d52f9029d
jaxon <jaxonlloyd05@gmail.com>
2026-04-07 13:04:35 +1000

    changed everything
    stack updated to use astrojs, removed bun due to no support with cloudflare pages, completely new look

---
+++ src/assets/svgs/index.js
@@ -0,0 +1,24 @@
+import gh from './github.svg';
+import li from './linkedin.svg';
+import en from './envelope.svg';
+import ts from './typescript.svg';
+import re from './react.svg';
+import sv from './svelte.svg';
+import aw from './aws.svg';
+import az from './azure.svg';
+import py from './python.svg';
+import dk from './docker.svg';
+
+
+export {
+  gh as GitHubIcon,
+  li as LinkedInIcon,
+  en as EnvelopeIcon,
+  ts as TypeScriptIcon,
+  re as ReactIcon,
+  sv as SvelteIcon,
+  aw as AWSIcon,
+  az as AzureIcon,
+  py as PythonIcon,
+  dk as DockerIcon
+}
